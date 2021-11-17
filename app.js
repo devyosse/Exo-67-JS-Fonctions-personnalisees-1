21d46c873cfb1492031b7576f0d790eb49473874
devyosse <narutoshippuden884@gmail.com>
2021-11-17 10:55:16 +0100

    exo 67

---
+++ app.js
@@ -0,0 +1,9 @@
+function mafonction(id, text){
+    document.getElementById(id).innerHTML = text;
+}
+
+mafonction('p1', 'Bienvenue');
+mafonction('p2', 'dans');
+mafonction('p3', 'J');
+mafonction('p4', 'S');
+
